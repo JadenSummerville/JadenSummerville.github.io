e047624f67389b1745b07c2f33a13eead7ebf636
JadenSummerville <79168451+JadenSummerville@users.noreply.github.com>
2023-02-03 22:08:20 -0800

    Adjusted friction

--- projects/pathFind.js
+++ projects/pathFind.js
@@ -113,7 +113,7 @@ class LineSegment {
         var x_V = x-this.position_x;
         var y_V = y-this.position_y;
         var hypotenus = Math.pow(x_V*x_V+y_V*y_V,.5);
-        if(hypotenus < 5){
+        if(hypotenus < 25){
             return;
         }
         this.velocity_x += x_V/hypotenus;
@@ -157,10 +157,10 @@ for(let i = 0; i < 100; i++){
 }
 console.log(a.position_x,a.position_y);
 */
-const agent = new Agent(500, 500, 1000, 0, 1000, 0, 0.5, 2);
+const agent = new Agent(500, 500, 1000, 0, 1000, 0, 0.99, 2);
 function steps(){
     agent.move();
-    agent.aimAt(0,300);
+    agent.aimAt(100,300);
     document.getElementById("agent").style.top = agent.position_y+"px";
     document.getElementById("agent").style.left = agent.position_x+"px";
 }
